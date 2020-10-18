991018a04a03897a0bc691ca751af21c4d7883ff
AndrewXiong2097 <71185802+AndrewXiong2097@users.noreply.github.com>
2020-10-18 10:46:33 -0500

    Add files via upload

---
+++ variablesExercise.py
@@ -0,0 +1,114 @@
+'''
+This is an exercise drill that you can try if you want to practice making
+more variables. 
+'''
+
+#1) Make an int variable named one, set to 4. And print the variable.
+
+
+
+#2) Make an int variable named two, set to 5. And print the variable.
+
+
+
+#3) Make an int variable named three, set to 132435. And print the variable.
+
+
+
+#4) Make an int variable named four, set to 90. And print the variable.
+
+
+
+#5) Make an int variable named five, set to the variable four. And print the 
+#variable.
+
+
+
+#6) Make a string variable named alpha, set to "one". And print the variable.
+
+
+
+#7) Make a string variable named bravo, set to "OMG". And print the variable.
+
+
+
+#8) Make a string variable named M, set to the second letter of bravo. And print
+#the variable. HINT: Uses indexes.
+
+
+
+#9) Make a string variable named charlie, set to "charlie". And print the 
+#variable.
+
+
+
+#10) Make a string variable named delta, set to charlie. And print the variable.
+
+
+
+#11) Make a boolean variable named booleanOne, set to True. And print the
+#variable.
+
+
+
+#12) Make a boolean variable named booleanTwo, set to False. And print the
+#variable.
+
+
+
+#13) Make a boolean variable named booleanThree, set to True. And print the
+#variable.
+
+
+
+#14) Make a boolean variable named booleanFour, set to False. And print the
+#variable.
+
+
+#15) Make a boolean variable named booleanFive, set to booleanFour. And print 
+#the variable.
+
+
+
+#16) Make a float variable named mono, set to 1.1. And print the variable.
+
+
+
+#17) Make a float variable named di, set to 2.2. And print the variable.
+
+
+
+#18) Make a float variable named tri, set to 3.3. And print the variable.
+
+
+
+#19) Make a float variable named quad, set to 4.4. And print the variable.
+
+
+
+#20) Make a float variable named var, set to quad. And print the variable.
+
+
+
+#21) Make a list named listOne, set to [1, 2, 3, 4, 5]. And print the list.
+
+
+
+#22) Make a list named listTwo, set to ["string", "string", "string"]. And print
+#the list.
+
+
+
+#23) Make a list named subList, set to the first three items of listOne. USE
+#INDEXES. And print the list.
+
+
+
+#24) Make a list named newList, set to listTwo. And print the list.
+
+
+
+#25) Make a list named boolean, set to [True, False, True, False]. And print the
+#list.
+
+
---
+++ variablesOutline.py
@@ -0,0 +1,65 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+One = 200
+a = True
+mango = "strawberry"
+ang = 3.55
+b = [1, 2, 3, 4, 5]
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+vara = 2230
+varA = 2231
+
+
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+ad = 3.52
+
+
+ad = 3.22
+
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+k = a
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+kikalo = ["pizza", "playstationFour", "chessBoard"]
+x = "playstationFour"
+
+
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first item
+#in that list:
+
+stringOne = "Andrew"
+v = "Andrew"
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+print(ang)
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+= 234
