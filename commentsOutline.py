d6ac4180208391ba8c73c1bd87be5c99b3906b51
AndrewXiong2097 <71185802+AndrewXiong2097@users.noreply.github.com>
2020-10-18 11:32:14 -0500

    Add files via upload

---
+++ commentsOutline.py
@@ -0,0 +1,22 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#Make a single line comment
+
+#2) Make a multi-line comment.
+
+'''
+I should really start cooking
+because I am craving a lot of different types of food
+and I don't want to go out or order them.
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+
+four = 36  #I just recently took my math test and got 36/48 :(
