de83c10eba1de48f215ed9d7ac25d88376a9f1de
AndrewXiong2097 <71185802+AndrewXiong2097@users.noreply.github.com>
2020-10-18 11:30:35 -0500

    Add files via upload

---
+++ commentDrills.py
@@ -0,0 +1,45 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#Make a single line comment
+
+#2) Make a multi-line comment.
+
+'''
+Hello
+I am hungry 
+..................
+What should I eat?
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+a = 3  #Math is hard but looking at this is harder
+
+#4) Make a single line comment.
+
+#Let's do this
+
+#5) Make a multi-line comment.
+
+'''
+I want to go out and play some volleyball.
+It will be fun to see my friends again after a long time.
+I enjoyed having fun with my friends especially when we play volleyball.
+'''
