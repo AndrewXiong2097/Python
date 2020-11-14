19b7c61c21408709e0d6005bef6b15dc08194d8d
AndrewXiong2097 <71185802+AndrewXiong2097@users.noreply.github.com>
2020-11-14 10:56:27 -0600

    Add files via upload

---
+++ Comparison Operators/comparisonOutline.py
@@ -0,0 +1,56 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+fa = "Mango"
+Ma = "Mango"
+x = "Mango" == "Mango"
+print(x)
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+one = 23
+two = 23
+z = 23 == 23
+print(z)
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+
+four = 10
+five = 5
+c = 10 > 5
+print(c)
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+
+six = 23
+seven = 28
+v = 23 < 28
+print(v)
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+
+eight = 123
+nine = x 
+b = 123 >= x 
+print(b)
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+
+ten = 123
+eleven = x 
+n = 123 <= x 
+print(n)
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+
+ <=4
---
+++ Mathematical Operators/mathOutline.py
@@ -0,0 +1,86 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+
+string = "string"
+stick = "stick"
+e = "string" + "stick"
+
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+
+one = 201
+two = 202
+s = one + two
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+
+three = 202
+four = 201
+q = three - four
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+bu = 23.5
+su = 24.5
+g = bu - su
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+
+five = 12
+six = 12
+l = five * six
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+
+lol = "Ha"
+seven = 4
+p = lol * seven
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+
+eight = 8
+nine = 7
+u = eight / nine 
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+
+la = 1.2
+ba = 3.2
+x = la / ba
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+
+ten = 200
+eleven = 120
+m = ten % eleven
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+
+twelve = 1202
+thirteen = 3606
+t = twelve // thirteen
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+
+fourteen = 1212
+fifteen = 1313
+z = fourteen ** fifteen
+
+#12) Make any type of operation with a common SYNTAX error
+
+product = lit[0] * list[1] * list[2]
