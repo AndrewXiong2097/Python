c1956ec6739b75016bd406803585c23b2c36381e
AndrewXiong2097 <71185802+AndrewXiong2097@users.noreply.github.com>
2021-02-04 21:16:30 -0600

    Add files via upload

---
+++ logicalOutline.py
@@ -0,0 +1,36 @@
+'''
+This outline will help solidify concepts from the Logical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+#EX) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+one = True
+two = False
+a = one and two 
+print(a)
+
+#1) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+three = True
+four = False
+a = three and four 
+print(a)
+
+
+#2) Make two boolean variables. Put them on either side of the or operator.
+#Store this expression in a variable named b. Print the variable.
+five = 5>3
+six = 8>5
+b = five or six 
+print(b)
+
+#3) Make one boolean variable. Put the variable after the not. Store this 
+#expression in a variable named c. Print the variable.
+seven = 5 == 6
+c = not seven 
+print(c)
+
+
+#4) Make a logical expression with one of the common SYNTAX errors.
+
+
